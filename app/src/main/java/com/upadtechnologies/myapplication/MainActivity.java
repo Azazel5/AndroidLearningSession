ca40bc3494080535f1bba4b9e13063ea0b98ebfa
subhanga <subhanga2013@gmail.com>
2019-02-23 15:07:37 -0600

    Activity LifeCycle

--- app/src/main/java/com/upadtechnologies/myapplication/MainActivity.java
+++ app/src/main/java/com/upadtechnologies/myapplication/MainActivity.java
@@ -2,28 +2,56 @@ package com.upadtechnologies.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int counter = 0;
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button myButton = findViewById(R.id.my_button);
-        final TextView textView = findViewById(R.id.my_text_view);
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                textView.setText("");
-                counter++;
-                textView.setText(Integer.toString(counter));
-
+                Toast.makeText(view.getContext(), R.string.str_2, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume()");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy()");
     }
 }
